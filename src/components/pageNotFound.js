16008d70b7f252dada87ade2761b956d1f7e2b3a
AbdulrhmanJo <Aalhani19@gmail.com>
2020-02-12 00:05:03 +0300

    add page not found component

---
+++ src/components/pageNotFound.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import pageNotFound from '../icon/page_not_found.svg'
+const PageNotFound = (props) => {
+    return (
+        <div className="notFound">
+            <h1>Oops!</h1>
+            <p className= 'info'>The question you are looking for, does not exist.</p>
+            <div>
+                <img src={pageNotFound} alt='page not found'></img>
+                <p>404 · Page not found</p>
+                <Link className="return-btn"  to='/'>Return</Link>
+            </div>
+        </div>
+    )
+}
+
+export default PageNotFound
--- src/components/pollPage.js
+++ src/components/pollPage.js
@@ -3,13 +3,19 @@ import { connect } from 'react-redux'
 import PollQuestion from "./PollQuestion";
 import PollResault from "./PollResault";
 import PollCard from './pollCard'
+import PageNotFound from './pageNotFound'
 class PollPage extends Component {
-    render(){        
+    render(){   
+        
+        if(!this.props.question){
+            return <PageNotFound />
+        }
+
         const { name, avatarURL,questions } = this.props.user 
         const { id } = this.props.match.params
         const { answers} = this.props.authedUserInfo
-
         return (
+            this.props.question ?
             <div>
                 <img src={avatarURL} alt={`avatar of ${name}`} className="question-avatar"/>
                 <p className="question-heading">Would you rather ?</p>
@@ -30,14 +36,19 @@ class PollPage extends Component {
                 }
                 
             </div>
+            : null
         )
     }
 }
 
 const mapStateToProp = ({ questions, authedUser, users}, { match }) => {
-    const question = questions[match.params.id]
-    const user = users[question.author]    
-    const authedUserInfo = users[authedUser]
+    const question = questions[match.params.id]    
+    let user, authedUserInfo;
+    if(question){
+        user = users[question.author]    
+        authedUserInfo = users[authedUser]
+    }
+    
     return {
         question,
         user,
